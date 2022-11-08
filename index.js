ceda3f9c2e299d5b648c1522bb4edeb81ce67a5f
Ibtisam34 <108012550+Ibtisam34@users.noreply.github.com>
2022-11-07 20:18:13 -0800

    added date

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import './modules/luxon.js';
-import './modules/Arrow.js';
+import { DateTime } from './modules/luxon.js';
+
+const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+document.getElementById('current-date').innerHTML = currentDate;
 
 
